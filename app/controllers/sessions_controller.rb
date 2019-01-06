43ef50995639cfdf31a9cbd76713d418fb3d375c
Max Holland <holla1144@gmail.com>
2019-01-06 13:54:11 +0200

    add helpers for logging in user, first part of user links dropdown

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid email/password combination' # show error messages
+      render 'new'
+    end
+  end
+
   def new
+    render 'new'
+  end
+
+  def destory
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,16 @@
 module SessionsHelper
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
+  # logs in a user
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  # Returns true if user is logged in, false otherwise
+  def logged_in?
+    !current_user.nil?
+  end
 end
