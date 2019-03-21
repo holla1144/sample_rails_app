3cf224ea173d230ad0ff3c53ccedc365cc1974ce
Max Holland <holla1144@gmail.com>
2019-03-22 00:24:52 +0200

    update credentials

--- app/assets/config/initializers/carrier_wave.rb
+++ app/assets/config/initializers/carrier_wave.rb
@@ -6,7 +6,6 @@ if Rails.env.production?
 
     config.aws_credentials = {
         # Configuration for Amazon S3
-        :provider              => 'AWS',
         :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
         :aws_secret_access_key => ENV['S3_SECRET_KEY'],
         :region                => ENV['S3_REGION']
